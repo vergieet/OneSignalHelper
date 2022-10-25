93593d11670b311b94307d89d6be82bf49533752
Vergie Ericson T <vergieericsont@gmail.com>
2022-10-26 05:13:32 +0700

    Add new $base_url, reformat code (#5)

--- OneSignalHelper.php
+++ OneSignalHelper.php
@@ -1,7 +1,9 @@
 <?php
+
 /**
  * Vergie's Framework 
  */
+
 namespace Vetx;
 
 class OneSignalHelper
@@ -17,98 +19,102 @@ class OneSignalHelper
      */
     protected $app_id;
     protected $rest_api_key;
-
+    protected $base_url = "https://onesignal.com/api/v1";
+    
     /* use for explode player ids  */
     protected $player_delimiter;
-    public function __construct($app_id,$rest_api_key)
+    public function __construct($app_id, $rest_api_key)
     {
-        $this->app_id = $app_id; 
+        $this->app_id = $app_id;
         $this->rest_api_key = $rest_api_key;
         $this->default_template = '';
         $this->player_delimiter = '#';
     }
 
-    public function setPlayerDelimiter($delimiter){
+    public function setPlayerDelimiter($delimiter)
+    {
         $this->player_delimiter = $delimiter;
     }
-    
-    public function sendToAll($message,$template_id=''){
+
+    public function sendToAll($message, $template_id = '')
+    {
         $content = array(
             "en" => $message
+        );
+        if ($template_id == '') {
+            $fields = array(
+                'app_id' => $this->app_id,
+                'included_segments' => array('All'),
+                'contents' => $content
             );
-        if($template_id ==''){
-        $fields = array(
-            'app_id' => $this->app_id,
-            'included_segments' => array('All'),
-            'contents' => $content
-        );
-       }else{
-        $fields = array(
-            'app_id' => $this->app_id,
-            'included_segments' => array('All'),
-            'contents' => $content,
-            'template_id' => $template_id
-        );
+        } else {
+            $fields = array(
+                'app_id' => $this->app_id,
+                'included_segments' => array('All'),
+                'contents' => $content,
+                'template_id' => $template_id
+            );
         }
-        
+
         $response = $this->request($fields);
-        
+
         return $response;
     }
 
-     public function sendToUser($message,$player_id,$template_id=''){
+    public function sendToUser($message, $player_id, $template_id = '')
+    {
         $content = array(
             "en" => $message
+        );
+        if ($template_id == '') {
+            $fields = array(
+                'app_id' => "",
+                'include_player_ids' => array($player_id),
+                'contents' => $content
             );
-        if($template_id ==''){
-        $fields = array(
-            'app_id' => "",
-            'include_player_ids' => array($player_id),
-            'contents' => $content
-        );
-       }else{
-        $fields = array(
-            'app_id' => $this->app_id,
-            'include_player_ids' => array($player_id),
-            'contents' => $content,
-            'template_id' => $template_id
-        );
+        } else {
+            $fields = array(
+                'app_id' => $this->app_id,
+                'include_player_ids' => array($player_id),
+                'contents' => $content,
+                'template_id' => $template_id
+            );
         }
-        
+
         $response = $this->request($fields);
-        
+
         return $response . $player_id;
     }
-    
-     public function sendToUsers($message,$player_ids,$template_id=''){
-        $player_ids = explode($this->player_delimiter, $player_ids)
-        $content = array(
-            "en" => $message
+
+    public function sendToUsers($message, $player_ids, $template_id = '')
+    {
+        $player_ids = explode($this->player_delimiter, $player_ids);
+        $content = array("en" => $message);
+        if ($template_id == '') {
+            $fields = array(
+                'app_id' => "",
+                'include_player_ids' => $player_ids,
+                'contents' => $content
             );
-        if($template_id ==''){
-        $fields = array(
-            'app_id' => "",
-            'include_player_ids' => $player_ids,
-            'contents' => $content
-        );
-       }else{
-        $fields = array(
-            'app_id' => $this->app_id,
-            'include_player_ids' => $player_ids,
-            'contents' => $content,
-            'template_id' => $template_id
-        );
+        } else {
+            $fields = array(
+                'app_id' => $this->app_id,
+                'include_player_ids' => $player_ids,
+                'contents' => $content,
+                'template_id' => $template_id
+            );
         }
-         
+
         $response = $this->request($fields);
-        
+
         return $response . $player_id;
     }
-    
-    private function request($fields){
+
+    private function request($fields)
+    {
         $fields = json_encode($fields);
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+        curl_setopt($ch, CURLOPT_URL, $base_url . "/notifications");
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',   'Authorization: Basic ' . $this->rest_api_key));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
         curl_setopt($ch, CURLOPT_HEADER, FALSE);
@@ -120,5 +126,4 @@ class OneSignalHelper
         curl_close($ch);
         return $response;
     }
-
 }
